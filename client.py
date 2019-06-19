d95b47b6c025ec283b12c182e96ddf8681e8030c
Gabriel Kutsovsky <gkutsovsky@gmail.com>
2019-06-19 16:35:39 -0400

    all 200

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 import urllib.parse
+import json
 
 
 url = 'http://localhost:5000/api/login'
@@ -15,10 +16,31 @@ async def authenticate(session, url):
       print(resp.status)
       return await resp.text()
 
+async def get(session, url, headers):
+  async with session.get(url, headers=headers) as resp:
+    print('get', resp.status)
+    text = await resp.text()
+    return resp.status, text
+
+    return 
+
+async def get_headers(session):
+  response = await authenticate(session, login_url)
+  token = json.loads(response)
+  return {'Authorization': f'Bearer {token["access_token"]}'}
+
 async def main():
   async with aiohttp.ClientSession() as session:
-      token = await authenticate(session, login_url)
-  print(token)
+      headers = await get_headers(session)
+      # need headers Authorization: Bearer <access_token_value>
+      for session, url in ((session, url) for url in secrets):
+            status, response = await get(session, url, headers)
+            if status == 401:
+              headers = await get_headers(session)
+              status, response = await get(session, url, headers)
+            print(status, response)
+            
+  
 
 
 if __name__ == "__main__":
